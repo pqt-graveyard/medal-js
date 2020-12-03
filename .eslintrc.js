4126119c5aae689366f249f5b77e85127cd4546a
Austin Paquette <austin@paquette.io>
2020-12-03 13:20:57 -0800

    Update .eslintrc.js
    Remove `overrides` applied to files that do not exist in this project structure.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,6 @@ module.exports = {
   extends: '@pqt',
   env: {
     node: true,
-    "jest": true
+    jest: true,
   },
-  overrides: [
-    {
-      files: ['pages/**/*.tsx'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off'
-      },
-    },
-    {
-      files: ['next-env.d.ts'],
-      rules: {
-        'unicorn/prevent-abbreviations': 'off',
-      },
-    },
-  ],
 };
