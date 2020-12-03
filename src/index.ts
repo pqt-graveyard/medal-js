a234a0e3d44009d04e2240a96d9cc5417b919160
Austin Paquette <austin.paquette@forcir.com>
2020-12-02 17:32:08 -0800

    all sorts of goodies

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export const generatePublicKey = () => {
+  console.log("generatePublicKey")
+}
+export const getCategories = () => {
+  console.log("getCategories")
+}
+export const getLatest = () => {
+  console.log("getLatest")
+}
+export const getTrending = () => {
+  console.log("getTrending")
+}
+export const search = () => {
+  console.log("search")
+}
